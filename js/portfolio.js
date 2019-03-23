7947a10086bf1d638f3df89e9a1423cdf237ee14
Vibhanshu619 <40599489+Vibhanshu619@users.noreply.github.com>
2019-03-23 12:42:06 +0530

    Add files via upload

---
+++ js/portfolio.js
@@ -0,0 +1,8 @@
+$(".button-collapse").sideNav();
+jQuery(document).ready(function(){
+	jQuery('.skillbar').each(function(){
+		jQuery(this).find('.skillbar-bar').animate({
+			width:jQuery(this).attr('data-percent')
+		},6000);
+	});
+}); 
